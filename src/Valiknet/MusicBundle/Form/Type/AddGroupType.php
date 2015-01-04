16853e9743e6755bd9a642c161e539d5b27f1fa5
Valik Grinevich <valik.v1per@gmail.com>
2015-01-04 03:12:40 +0200

    update form and controller

--- src/Valiknet/MusicBundle/Form/Type/AddGroupType.php
+++ src/Valiknet/MusicBundle/Form/Type/AddGroupType.php
@@ -1,6 +1,7 @@
 <?php
 namespace Valiknet\MusicBundle\Form\Type;
 
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
@@ -11,31 +12,54 @@ class AddGroupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('poster', 'file', [
+                "label" => "Постер группи",
+                "required" => true
+            ])
             ->add('name', 'text', [
-                "label" => "Назва групи"
+                "label" => "Назва групи",
+                "required" => true
             ])
-            ->add('poster', 'file')
             ->add('history', 'textarea', [
-                "label" => "Інформація про групу"
+                "label" => "Інформація про групу",
+                "required" => true
             ])
-            ->add('officialSite', 'text', [
-                "label" => "Офіційний сайт"
+            ->add('officialSite', 'url', [
+                "label" => "Офіційний сайт",
+                "required" => false
             ])
-            ->add('officialVkPage', 'text', [
-                "label" => "Офіційна сторінка вконтакті"
+            ->add('officialVkPage', 'url', [
+                "label" => "Офіційна сторінка вконтакті",
+                "required" => false
             ])
-            ->add('officialTwitterPage', 'text', [
-                "label" => "Офіційна сторінка в твіттері"
+            ->add('officialTwitterPage', 'url', [
+                "label" => "Офіційна сторінка в твіттері",
+                "required" => false
             ])
-            ->add('officialFacebookPage', 'text', [
-                "label" => "Офіційна сторінка в фейсбукі"
+            ->add('officialFacebookPage', 'url', [
+                "label" => "Офіційна сторінка в фейсбукі",
+                "required" => false
             ])
             ->add('country', 'entity', [
                 "class" => "ValiknetMusicBundle:Country",
                 "empty_value" => "Виберіть потрібну країну",
+                "required" => true
             ])
-            ->add('createdAt', 'date')
-            ->add('styles');
+            ->add('createdAt', 'date', [
+                "label" => "Дата створення группи",
+                "required" => true,
+                "years" => range(1950, 2015),
+            ])
+            ->add("styles", 'entity', [
+                "class" => 'ValiknetMusicBundle:Style',
+                "query_builder" => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('s')
+                            ->where('s.parent is NULL');
+                },
+                "property" => "name",
+                "multiple" => "true",
+                "expanded" => true
+            ]);
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
