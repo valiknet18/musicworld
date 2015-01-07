6a2fa26a7d26f2b67cfd100e8c31755e14c8446b
Valik Grinevich <valik.v1per@gmail.com>
2015-01-07 13:36:08 +0200

    add form type for add country

---
+++ src/Valiknet/MusicBundle/Form/Type/CountryType.php
@@ -0,0 +1,30 @@
+<?php
+namespace Valiknet\MusicBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Valiknet\MusicBundle\Form\DataTransformer\UrlTransformer;
+
+class CountryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', [
+                "label" => "Назва країни",
+            ]);
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Valiknet\MusicBundle\Entity\Country',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'add_clip';
+    }
+}
